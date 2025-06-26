db04901a84accdf7c47aae6c0f0ec18988f12db6
sshakibaa <sadmansakib25350@gmail.com>
2025-06-26 09:36:36 +0600

    .

--- EvenMangment.c
+++ EvenMangment.c
@@ -4,18 +4,20 @@
 
 struct user{
     char fullName[50];
-    char emaill[50];
+    char email[50];
     char password[50];
     char username[50];
     char phone[50];
 };
-
-void takeuserName()
-{
-
+void takeinput(char *str){
+    fgets(str, 50, stdin);
+    str[strcspn(str, "\n")] = 0; // Remove trailing newline character
 }
+
+
 int main(){
     int choice;
+    struct user user;
     printf("\t \t \t \t--------Welcome to Even Mangment System--------\t \t \t \t \n"); 
     printf("Main Menu");
     printf("\n1.Register");
@@ -24,20 +26,20 @@ int main(){
 
 
     printf("\nEnter your choice: ");
-    scanf("%d",&choice);
-    fgetc(stdin);
+scanf("%d",&choice);
+fgetc(stdin);
 
-    switch(choice){
-        case 1:
-            printf("Enter your full name: ");
-            takeinput(user.fullName);
-            printf("Enter your Emaill: ");
-            takeinput(user.email);
-            printf("Enter your contact: ");
-            takeinput(user.phone);
-            break;
+switch(choice){
+    case 1:
+        printf("Enter your full name: ");
+        takeinput(user.fullName);
+        printf("Enter your Email: ");
+        takeinput(user.email);
+        printf("Enter your contact: ");
+        takeinput(user.phone);
+        break;
 
-    }
+}
 
 
 

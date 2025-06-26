3d92d50f8170a91f1ded2fadc27d434b77b3e65c
sshakibaa <sadmansakib25350@gmail.com>
2025-06-26 08:55:15 +0600

    .

--- EvenMangment.c
+++ EvenMangment.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
+struct user{
+    char fullName[50];
+    char emaill[50];
+    char password[50];
+    char username[50];
+    char phone[50];
+};
 int main(){
     int choice;
     printf("Welcome to Even Mangment System\n"); 
